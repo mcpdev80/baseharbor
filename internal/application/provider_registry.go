f049bd64ccc0f5c8cbfb869fc3224f20b452495d
Marcel Pfingstgräf <dev@pfingstgraef.eu>
2026-09-18 19:19:24 +0200

    feat: map current providers into registry

---
+++ internal/application/provider_registry.go
@@ -0,0 +1,90 @@
+package application
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/mcpdev80/baseharbor/internal/capability"
+	bhruntime "github.com/mcpdev80/baseharbor/internal/runtime"
+)
+
+const providerRegistryFile = "provider-registry.json"
+
+func ReconcileReferenceProviderRegistry(m Manifest) error {
+	stateDir, err := bhruntime.StateDir("")
+	if err != nil {
+		return err
+	}
+	store := capability.RegistryStore{Path: filepath.Join(stateDir, providerRegistryFile)}
+	registry, err := store.Load()
+	if err != nil {
+		return err
+	}
+	registry.ReleaseApplication(m.Name)
+	if err := registerReferenceProviders(&registry, m); err != nil {
+		return err
+	}
+	return store.Save(registry)
+}
+
+func ReleaseApplicationProviderRegistry(m Manifest) error {
+	stateDir, err := bhruntime.StateDir("")
+	if err != nil {
+		return err
+	}
+	store := capability.RegistryStore{Path: filepath.Join(stateDir, providerRegistryFile)}
+	registry, err := store.Load()
+	if err != nil {
+		return err
+	}
+	registry.ReleaseApplication(m.Name)
+	return store.Save(registry)
+}
+
+func registerReferenceProviders(registry *capability.Registry, m Manifest) error {
+	contract, err := PortableContractFromManifest(m)
+	if err != nil {
+		return err
+	}
+	resources, err := ResolveCapabilityResources(contract)
+	if err != nil {
+		return err
+	}
+	for _, resource := range resources {
+		var instance capability.ProviderInstance
+		switch resource.Provider {
+		case capability.ProviderPostgreSQL:
+			instance = capability.ProviderInstance{
+				ID: fmt.Sprintf("postgresql/%s/%s/%s", m.Name, m.Environment, resource.Name),
+				Provider: capability.PostgreSQL,
+				Scope: capability.ScopeApplication,
+				Ownership: capability.OwnershipBaseHarbor,
+				OwnerApplication: m.Name,
+			}
+		case capability.ProviderValkey:
+			instance = capability.ProviderInstance{
+				ID: fmt.Sprintf("valkey/%s/%s/%s", m.Name, m.Environment, resource.Name),
+				Provider: capability.Valkey,
+				Scope: capability.ScopeApplication,
+				Ownership: capability.OwnershipBaseHarbor,
+				OwnerApplication: m.Name,
+			}
+		case capability.ProviderOpenBao:
+			instance = capability.ProviderInstance{
+				ID: "openbao/control-plane",
+				Provider: capability.OpenBao,
+				Scope: capability.ScopeShared,
+				Ownership: capability.OwnershipBaseHarbor,
+			}
+		default:
+			return fmt.Errorf("no reference provider registry mapping for %q", resource.Provider)
+		}
+		if err := registry.Register(instance); err != nil {
+			return err
+		}
+		if err := registry.Bind(resource, instance.ID); err != nil {
+			return err
+		}
+	}
+	return nil
+}
