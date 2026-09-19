cb7805d8c518cc05e8ca954e67cf9811620b5543
Marcel Pfingstgräf <dev@pfingstgraef.eu>
2026-09-19 07:03:30 +0200

    feat: add non-destructive Compose project stop primitive

--- internal/runtime/compose.go
+++ internal/runtime/compose.go
@@ -78,6 +78,10 @@ func (c Compose) UpProject(ctx context.Context, project, composeFile, envFile st
 	return c.runProject(ctx, project, composeFile, envFile, "up", "-d")
 }
 
+func (c Compose) StopProject(ctx context.Context, project, composeFile, envFile string) error {
+	return c.runProject(ctx, project, composeFile, envFile, "stop")
+}
+
 func (c Compose) DownProject(ctx context.Context, project, composeFile, envFile string) error {
 	return c.runProject(ctx, project, composeFile, envFile, "down")
 }
