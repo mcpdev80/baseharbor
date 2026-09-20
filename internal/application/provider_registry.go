339ca02f76a2556013fe83e8a6b5decf6be735a3
Marcel Pfingstgräf <dev@pfingstgraef.eu>
2026-09-20 03:54:48 +0200

    feat(v0.4.9): register platform provider resources through protected registry

--- internal/application/provider_registry.go
+++ internal/application/provider_registry.go
@@ -39,15 +39,56 @@ func CheckReferenceProviderRegistry(m Manifest) error {
 	return registry.Validate()
 }
 
-func ReconcileReferenceProviderRegistry(m Manifest) error {
+func ReconcileReferenceProviderRegistry(m Manifest, additional ...capability.Resource) error {
 	store, err := referenceProviderRegistryStore()
 	if err != nil {
 		return err
 	}
 	return store.Update(func(registry *capability.Registry) error {
 		registry.ReleaseManagedApplication(m.Name)
-		return registerReferenceProviders(registry, m)
+		if err := registerReferenceProviders(registry, m); err != nil {
+			return err
+		}
+		return registerAdditionalProviderResources(registry, m, additional)
 	})
+}
+
+func CheckAdditionalProviderResources(m Manifest, additional []capability.Resource) error {
+	store, err := referenceProviderRegistryStore()
+	if err != nil {
+		return err
+	}
+	registry, err := store.Load()
+	if err != nil {
+		return err
+	}
+	registry.ReleaseManagedApplication(m.Name)
+	if err := registerReferenceProviders(&registry, m); err != nil {
+		return err
+	}
+	if err := registerAdditionalProviderResources(&registry, m, additional); err != nil {
+		return err
+	}
+	return registry.Validate()
+}
+
+func registerAdditionalProviderResources(registry *capability.Registry, m Manifest, resources []capability.Resource) error {
+	for _, resource := range resources {
+		if resource.Application != m.Name {
+			return fmt.Errorf("additional provider resource belongs to application %q, expected %q", resource.Application, m.Name)
+		}
+		instance, err := referenceProviderInstance(m, resource)
+		if err != nil {
+			return err
+		}
+		if err := registry.Register(instance); err != nil {
+			return err
+		}
+		if err := registry.Bind(resource, instance.ID); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func CheckControlPlaneDestroySafe() error {
