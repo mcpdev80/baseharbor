0f1e5b28062ace9a72c0e9dea61a810a5997cb11
Marcel Pfingstgräf <dev@pfingstgraef.eu>
2026-09-21 12:59:34 +0200

    fix: detect multi-environment repositories at root

--- cmd/baha/commands.go
+++ cmd/baha/commands.go
@@ -193,6 +193,6 @@ func inApplicationRepository() bool {
 	if err != nil {
 		return false
 	}
-	_, err = application.FindRepositoryManifest(cwd)
-	return err == nil
+	found, err := application.HasRepositoryApplication(cwd)
+	return err == nil && found
 }
