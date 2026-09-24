9eed27028d1e929bb73f69adce98ceafd7627668
Marcel Pfingstgräf <dev@pfingstgraef.eu>
2026-09-24 13:57:18 +0200

    test(trust): remove duplicate host trust opt-in assertion

--- cmd/baha/runtime_ports_test.go
+++ cmd/baha/runtime_ports_test.go
@@ -35,16 +35,6 @@ func TestRuntimeUpYesDoesNotImplicitlyTrustHostCA(t *testing.T) {
 	}
 }
 
-func TestParseRuntimeUpOptionsYesDoesNotImplyHostTrust(t *testing.T) {
-	opts, err := parseRuntimeUpOptions([]string{"--yes"})
-	if err != nil {
-		t.Fatal(err)
-	}
-	if opts.TrustHostCA {
-		t.Fatal("--yes must never imply host trust installation")
-	}
-}
-
 func TestParseRuntimeUpOptionsAcceptsRecoveryFileEqualsForm(t *testing.T) {
 	opts, err := parseRuntimeUpOptions([]string{"--recovery-file=/secure/recovery.json"})
 	if err != nil {
