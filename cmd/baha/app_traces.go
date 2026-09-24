808d193626cb9ba775d9803d9a7fa0b1ec676d74
Marcel Pfingstgräf <dev@pfingstgraef.eu>
2026-09-24 12:50:59 +0200

    refactor(pki): inject issuer into traces preparation

--- cmd/baha/app_traces.go
+++ cmd/baha/app_traces.go
@@ -9,6 +9,7 @@ import (
 	"github.com/mcpdev80/baseharbor/internal/capability"
 	bhruntime "github.com/mcpdev80/baseharbor/internal/runtime"
 	"github.com/mcpdev80/baseharbor/internal/serviceaccess"
+	"github.com/mcpdev80/baseharbor/internal/serviceaccess"
 	tracesprovider "github.com/mcpdev80/baseharbor/internal/traces"
 )
 
