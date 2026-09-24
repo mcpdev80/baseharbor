e8128a6335b4afc8ce77eee5397b109807fea4b7
Marcel Pfingstgräf <dev@pfingstgraef.eu>
2026-09-24 11:41:57 +0200

    feat(runtime): track TLS access containers as owned resources

--- internal/application/runtime_lifecycle.go
+++ internal/application/runtime_lifecycle.go
@@ -23,6 +23,7 @@ func ExpectedRuntimeResources(m Manifest) []bhruntime.ProjectResource {
 		service := runtimeServiceName("postgres", instance)
 		resources = append(resources,
 			bhruntime.ProjectResource{Kind: "container", Name: project + "-" + service + "-1"},
+			bhruntime.ProjectResource{Kind: "container", Name: project + "-" + postgresAccessService(instance) + "-1"},
 			bhruntime.ProjectResource{Kind: "volume", Name: project + "_" + service + "-data"},
 		)
 	}
@@ -30,6 +31,7 @@ func ExpectedRuntimeResources(m Manifest) []bhruntime.ProjectResource {
 		service := runtimeServiceName("valkey", instance)
 		resources = append(resources,
 			bhruntime.ProjectResource{Kind: "container", Name: project + "-" + service + "-1"},
+			bhruntime.ProjectResource{Kind: "container", Name: project + "-" + valkeyAccessService(instance) + "-1"},
 			bhruntime.ProjectResource{Kind: "volume", Name: project + "_" + service + "-data"},
 		)
 	}
